54b814f74975a984638facb6fc18d0f855aca2ba
NguyenHieu09 <hieu92145@gmail.com>
2024-04-25 20:32:04 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Login from './screens/login/login'
+
+const App = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  )
+}
+export default App
+
