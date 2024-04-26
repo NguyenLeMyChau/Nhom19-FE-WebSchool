b1145c3cdb8e367bdeef6152108b46f21c0a4b89
My Chau <nguyenlemychau1201@gmail.com>
2024-04-26 22:04:01 +0700

    Create UI Login

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import './login.css'
 
 function Login() {
   return (
-    <div>
-      kuhfks
+    <div className='body'>
+      <div className='login'>
+        <h1 style={{marginTop: 50, color: '#006AF5'}}>Login</h1>
+        <form className='login-form'>
+          <input type='text' placeholder='Username' className='input'/> <br/>
+          <input type='password' placeholder='Password' className='input'/> <br/>
+          <button className='button'>Login</button>
+        </form>
+      </div>
     </div>
+
   )
 }
 
